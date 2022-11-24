30a0dbf6d86af2cdc25774d66816bb9ce1108639
Md Anik Ahammed Khan <anikkhan1105@gmail.com>
2022-11-25 03:19:02 +0600

    all products load setup done

--- index.js
+++ index.js
@@ -5,9 +5,32 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-
 app.use(express.json());
 app.use(cors());
+
+const categories = require("./category.json");
+const allProducts = require("./allProducts.json");
+
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+});
+
+app.get('/products', (req, res) => {
+    res.send(allProducts);
+});
+
+// get product by brand 
+app.get('/products/:brand', (req, res) => {
+    const brand = req.params.brand;
+    const brandProducts = allProducts.filter(pd => pd.brand === brand);
+    res.send(brandProducts);
+});
+
+
+
+
+
 
 
 
